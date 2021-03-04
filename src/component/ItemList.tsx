d743f7a3567b610198a6cd31f96f680d6aabf733
sungyun <sungyun5423@gmail.com>
2021-03-05 00:21:29 +0900

    fix/data 개수 수정

--- src/component/ItemList.tsx
+++ src/component/ItemList.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "../styles/ItemList.scss";
-import data from "../data.json";
+import data from "../dummy.json";
 
 // const Item = () => data.map((e) => <div>{e.name}</div>);
 
 const ItemList = () => {
   const list = data.map((e) => (
     <div key={e._id} className="box">
-      <img src={e.imageUrl} />
+      <span>
+        <img src={e.imageUrl} />
+        <p>{e.gender}이다</p>
+      </span>
       <div>{e.name}</div>
       <span>{e.age}살 입니다</span>
       <button>삭제</button>
