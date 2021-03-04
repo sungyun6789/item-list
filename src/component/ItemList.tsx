7c274f2dd348c3499e55d4e5dbcb427043bb579b
sungyun <sungyun5423@gmail.com>
2021-03-05 00:17:20 +0900

    feat/get json-data list

--- src/component/ItemList.tsx
+++ src/component/ItemList.tsx
@@ -1,8 +1,26 @@
 import React from "react";
-import axios from "axios";
+import "../styles/ItemList.scss";
+import data from "../data.json";
 
-const ItemList: React.FC = () => {
-  return <div>아이템 목록</div>;
+// const Item = () => data.map((e) => <div>{e.name}</div>);
+
+const ItemList = () => {
+  const list = data.map((e) => (
+    <div key={e._id} className="box">
+      <img src={e.imageUrl} />
+      <div>{e.name}</div>
+      <span>{e.age}살 입니다</span>
+      <button>삭제</button>
+    </div>
+  ));
+
+  return (
+    <div className="item">
+      <div className="item-container">
+        <div>{list}</div>
+      </div>
+    </div>
+  );
 };
 
 export default ItemList;
