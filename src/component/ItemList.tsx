c02fc784d53b5fbf402556964010dcd490045044
sungyun <sungyun5423@gmail.com>
2021-03-07 00:24:53 +0900

    feat: remove button

--- src/component/ItemList.tsx
+++ src/component/ItemList.tsx
@@ -1,23 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/ItemList.scss";
 import data from "../dummy.json";
 
 const ItemList = () => {
-  const removeClick = (e: React.MouseEvent) => {
-    console.log(e);
+  const [list, setList] = useState(data);
+
+  const removeClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setList(list.filter((data) => data._id !== e.currentTarget.value));
   };
+
+  useEffect(() => console.log(list), [list]);
 
   return (
     <div className="item">
       <h4>안녕하세요! 관리자님!</h4>
       <div className="item-container">
-        {data.map((item) => (
+        {list.map((item) => (
           <div key={item._id} className="box">
             <img src={item.imageUrl} className="img-box" alt="고양이" />
             <h3 className="name">{item.name}</h3>
             <span className="age">{item.age}살 입니다</span>
             <div className="button-box">
-              <button onClick={removeClick}>삭제</button>
+              <button onClick={removeClick} value={item._id}>
+                삭제
+              </button>
             </div>
           </div>
         ))}
