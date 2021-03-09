72cdf3f8f13a71cd4c37f88bc67ac56f3f13c447
sungyun <sungyun5423@gmail.com>
2021-03-09 19:18:51 +0900

    refactor: ui refactoring

--- src/component/ItemList.tsx
+++ src/component/ItemList.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState, useCallback } from "react";
-import "../styles/ItemList.scss";
-import data from "../data.json";
+import React, { useEffect, useState, useCallback } from 'react';
+import '../styles/ItemList.scss';
+import data from '../data.json';
 
 interface ItemTypes {
   _id: string;
@@ -11,19 +11,13 @@ interface ItemTypes {
 }
 
 const ItemList = () => {
-  const [itemIndex, setItemIndex] = useState(0);
+  const [itemIndex, setItemIndex] = useState<number>(0);
   const [result, setResult] = useState<any>(data.slice(0, 20));
-  const [index, setIndex] = useState(Math.floor(Math.random() * 15 + 5));
+  const [index, setIndex] = useState<number>(Math.floor(Math.random() * 15 + 5));
 
   const infiniteScroll = useCallback(() => {
-    let scrollHeight = Math.max(
-      document.documentElement.scrollHeight,
-      document.body.scrollHeight
-    );
-    let scrollTop = Math.max(
-      document.documentElement.scrollTop,
-      document.body.scrollTop
-    );
+    let scrollHeight = Math.max(document.documentElement.scrollHeight, document.body.scrollHeight);
+    let scrollTop = Math.max(document.documentElement.scrollTop, document.body.scrollTop);
     let clientHeight = Math.max(document.documentElement.clientHeight);
 
     if (scrollTop + clientHeight === scrollHeight) {
@@ -39,14 +33,12 @@ const ItemList = () => {
 
   useEffect(() => {
     RandomBox();
-    window.addEventListener("scroll", infiniteScroll, true);
-    return () => window.removeEventListener("scroll", infiniteScroll, true);
+    window.addEventListener('scroll', infiniteScroll, true);
+    return () => window.removeEventListener('scroll', infiniteScroll, true);
   }, [infiniteScroll, result]);
 
   const removeClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    setResult(
-      result.filter((data: ItemTypes) => data._id !== e.currentTarget.value)
-    );
+    setResult(result.filter((data: ItemTypes) => data._id !== e.currentTarget.value));
   };
 
   return (
@@ -59,13 +51,13 @@ const ItemList = () => {
           <div key={item._id} className="box">
             {item.name === undefined ? (
               <div className="randomBox">
-                <p>사지말고 입양하세요</p>
+                <p>사지 말고 입양 하세요!</p>
               </div>
             ) : (
               <>
                 <img src={item.imageUrl} alt="고양이" />
                 <h3 className="name">{item.name}</h3>
-                {item.gender === "female" ? (
+                {item.gender === 'female' ? (
                   <span className="img-text">암컷이다냥</span>
                 ) : (
                   <span className="img-text">수컷이다냥</span>
