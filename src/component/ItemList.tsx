daa4b0149096560094490d7579dbca1e86f5500c
sungyun <sungyun5423@gmail.com>
2021-03-09 19:06:36 +0900

    feat: random div, ui refactoring

--- src/component/ItemList.tsx
+++ src/component/ItemList.tsx
@@ -13,6 +13,7 @@ interface ItemTypes {
 const ItemList = () => {
   const [itemIndex, setItemIndex] = useState(0);
   const [result, setResult] = useState<any>(data.slice(0, 20));
+  const [index, setIndex] = useState(Math.floor(Math.random() * 15 + 5));
 
   const infiniteScroll = useCallback(() => {
     let scrollHeight = Math.max(
@@ -31,57 +32,56 @@ const ItemList = () => {
     }
   }, [itemIndex, result]);
 
+  const RandomBox = () => {
+    result.splice(index, 0, <div></div>);
+    setIndex(index + result.length);
+  };
+
+  useEffect(() => {
+    RandomBox();
+    window.addEventListener("scroll", infiniteScroll, true);
+    return () => window.removeEventListener("scroll", infiniteScroll, true);
+  }, [infiniteScroll, result]);
+
   const removeClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     setResult(
       result.filter((data: ItemTypes) => data._id !== e.currentTarget.value)
     );
   };
 
-  useEffect(() => {
-    window.addEventListener("scroll", infiniteScroll, true);
-    return () => window.removeEventListener("scroll", infiniteScroll, true);
-  }, [result, infiniteScroll]);
-
-  // random div 미완
-  const addRandomBox = () => {
-    const randomIndex = Math.random() * 15 + 5;
-    while (randomIndex < result.length) {
-      result.splice(
-        randomIndex,
-        0,
-        <div className="randomBox">사지말고 입양하세요</div>
-      );
-    }
-  };
-
   return (
-    <>
-      <div className="item">
-        <header>
-          <h4>안녕하세요! 관리자님!</h4>
-        </header>
-        <div className="item-container">
-          {result.map((item: ItemTypes) => (
-            <div key={item._id} className="box">
-              {addRandomBox}
-              <img src={item.imageUrl} alt="고양이" />
-              <h3 className="name">{item.name}</h3>
-              {item.gender === "female" ? (
-                <span className="img-text">암컷이다냥</span>
-              ) : (
-                <span className="img-text">수컷이다냥</span>
-              )}
-              <span className="age">{item.age}살 입니다</span>
-              <div className="button-box">
-                <button onClick={removeClick} value={item._id}>
-                  삭제
-                </button>
+    <div className="item">
+      <header>
+        <h4>안녕하세요! 관리자님!</h4>
+      </header>
+      <div className="item-container">
+        {result.map((item: ItemTypes) => (
+          <div key={item._id} className="box">
+            {item.name === undefined ? (
+              <div className="randomBox">
+                <p>사지말고 입양하세요</p>
               </div>
-            </div>
-          ))}
-        </div>
+            ) : (
+              <>
+                <img src={item.imageUrl} alt="고양이" />
+                <h3 className="name">{item.name}</h3>
+                {item.gender === "female" ? (
+                  <span className="img-text">암컷이다냥</span>
+                ) : (
+                  <span className="img-text">수컷이다냥</span>
+                )}
+                <span className="age">{item.age}살 입니다</span>
+                <div className="button-box">
+                  <button onClick={removeClick} value={item._id}>
+                    삭제
+                  </button>
+                </div>
+              </>
+            )}
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
